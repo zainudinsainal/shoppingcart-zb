0a21e3a2f356d4f7f5ce0df9abe4b5a794f8b57d
Bernard <bernardleongqijie@gmail.com>
2018-03-05 17:31:06 +0800

    add order model and validations

---
+++ db/migrate/20180305091804_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :orders do |t|
+      t.integer :status, default: 0
+      t.date :order_date, null: false
+      t.float :total_price, null: false
+      t.bigint :order_num, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305091030) do
+ActiveRecord::Schema.define(version: 20180305091804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20180305091030) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "status", default: 0
+    t.date "order_date", null: false
+    t.float "total_price", null: false
+    t.bigint "order_num", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
