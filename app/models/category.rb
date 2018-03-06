ca658b0eac81e178721e0d79e053bb1e7c868a72
zainudinsainal <supernova37@gmail.com>
2018-03-06 14:27:36 +0800

    Fix bugs in admin product view

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   has_many :categories_products, dependent: :destroy
-  has_many :products, through: :categories
+  has_many :products, through: :categories_products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   validates :price, presence: true
 
   has_many :categories_products, dependent: :destroy
-  has_many :categories, through: :products
+  has_many :categories, through: :categories_products
 
   has_many :orders_products, dependent: :destroy
   has_many :orders, through: :orders_products
