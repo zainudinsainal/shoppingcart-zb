8b2341e9580506a1b495b62a264d71290b3f0607
zainudinsainal <supernova37@gmail.com>
2018-03-06 13:27:03 +0800

    Setup filestack api .env

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module ShoppingCart
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.filestack_rails.api_key = ENV["FILESTACK_API_KEY"]
+    
   end
 end
