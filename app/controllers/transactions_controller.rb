8c18c8770d3228414047f1146cfd3a2851b1527f
Bernard <bernardleongqijie@gmail.com>
2018-03-13 22:34:18 +0800

    fully implement mailer

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -9,25 +9,23 @@ class TransactionsController < ApplicationController
     @result = Braintree::Transaction.sale(
               amount: current_user.cart_total_price,
               payment_method_nonce: params[:payment_method_nonce])
-    if @result.success?
-      order = current_user.purchase_products!
-
-      TransactionMailer.paid(current_user, order).deliver_later 
-      order.create_payment(
-        payment_method: @result.transaction.payment_instrument_type, 
-        payment_status: @result.transaction.status, 
-        payment_amount: @result.transaction.amount, 
-        payment_type: @result.transaction.type, 
-        payment_response: @result.transaction.processor_response_text, 
-        user_id: current_user.id
-      )
-
+              order = current_user.purchase_products!
+              order.create_payment(
+                payment_method: @result.transaction.payment_instrument_type, 
+                payment_status: @result.transaction.status, 
+                payment_amount: @result.transaction.amount, 
+                payment_type: @result.transaction.type, 
+                payment_response: @result.transaction.processor_response_text, 
+                user_id: current_user.id
+              )
+              if @result.success?
+                TransactionMailer.paid(current_user, order).deliver_later 
       $redis.del(current_user.id)
       redirect_to orders_path, notice: "Congraulations! Your transaction has been successfully!"
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
+      TransactionMailer.unpaid(current_user, order).deliver_later
       gon.client_token = generate_client_token
-      TransactionMailer.unpaid(current_user).deliver_later
       render :new
     end
   end
