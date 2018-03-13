d1fc361b4ea4ecb51f4dc43ef6222d1f9b85bae6
Bernard <bernardleongqijie@gmail.com>
2018-03-13 16:19:11 +0800

    mailer success implemented

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -1,13 +1,12 @@
 class TransactionMailer < ApplicationMailer
   def paid(user, order)
-    #byebug
     @order = order
     @user = user
     mail to: @user.email, subject: "Welcome to the application"
   end
 
-  def unpaid(user)
-    @order = Order.find(params[:id])
+  def unpaid(user, order)
+    @order = order
     @user = user
     mail to: @user.email, subject: "Products unpaid."
   end
